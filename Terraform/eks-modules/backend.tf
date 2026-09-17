17c6eb4fd7ea329be2a20c706b164d1f59715d4b
Ishikapbhatt <ishikap1998@gmail.com>
2026-09-17 10:02:35 +0530

    Remove S3 backend to use local state

--- Terraform/eks-modules/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ishika-terraform-state"
-    key            = "eks-modules/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-  }
-}
